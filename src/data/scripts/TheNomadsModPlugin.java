c5810f9b51fe1798add0ad9840d865aa9ca2634b
T.W.R. Cole <trylobot@gmail.com>
2017-06-20 17:03:22 -0500

    Incorporated data changes from Snrasha
    Updated Faction/Crest art
    Updated ship hull data

--- src/data/scripts/TheNomadsModPlugin.java
+++ src/data/scripts/TheNomadsModPlugin.java
@@ -1,9 +1,6 @@
 package data.scripts;
 
 import com.fs.starfarer.api.BaseModPlugin;
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.SectorAPI;
-import data.scripts.trylobot.TrylobotUtils;
 
 
 public class TheNomadsModPlugin extends BaseModPlugin
