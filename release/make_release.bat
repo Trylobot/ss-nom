eaa267b5e92e03cec40b036bf3fee0721502c7fd
Trylobot <trylobot@gmail.com>
2017-07-03 14:01:22 -0500

    release script finished

---
+++ release/make_release.bat
@@ -0,0 +1,22 @@
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`json -f ..\mod_info.json version`) DO (
+SET version=%%F
+)
+
+echo mod_info.version: %version%
+
+del /F *.zip
+RMDIR /S /Q  ss-nom
+
+MKDIR        ss-nom
+XCOPY /S /I  ..\data           ss-nom\data
+XCOPY /S /I  ..\graphics       ss-nom\graphics
+XCOPY /S /I  ..\jars           ss-nom\jars
+XCOPY /S /I  ..\sounds         ss-nom\sounds
+XCOPY /S /I  ..\src            ss-nom\src
+COPY         ..\mod_info.json  ss-nom
+
+7z.exe a     ss-nom-%version%.zip  ss-nom 
+
+pause
+
