c206a09130ee747ce9e9b56fbe97e7088e120967
Trylobot <trylobot@gmail.com>
2017-07-07 23:41:52 -0500

    include version file in release pkg

--- release/make_release.bat
+++ release/make_release.bat
@@ -9,12 +9,13 @@ del /F *.zip
 RMDIR /S /Q  ss-nom
 
 MKDIR        ss-nom
-XCOPY /S /I  ..\data           ss-nom\data
-XCOPY /S /I  ..\graphics       ss-nom\graphics
-XCOPY /S /I  ..\jars           ss-nom\jars
-XCOPY /S /I  ..\sounds         ss-nom\sounds
-XCOPY /S /I  ..\src            ss-nom\src
-COPY         ..\mod_info.json  ss-nom
+XCOPY /S /I  ..\data            ss-nom\data
+XCOPY /S /I  ..\graphics        ss-nom\graphics
+XCOPY /S /I  ..\jars            ss-nom\jars
+XCOPY /S /I  ..\sounds          ss-nom\sounds
+XCOPY /S /I  ..\src             ss-nom\src
+COPY         ..\mod_info.json   ss-nom
+COPY         ..\nomads.version  ss-nom
 
 7z.exe a     ss-nom-%version%.zip  ss-nom 
 
